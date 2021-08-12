33a97395ca373b7936cc0f6d7638443e33b4b713
valliant71 <valliant71@yahoo.com>
2021-08-11 17:45:17 -1000

    added card system and props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Hello greeting={'Hello ' + 'Robopals Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+              <div>
+                <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+              </div>              
+, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
